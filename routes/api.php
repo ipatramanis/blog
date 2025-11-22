2d16c1fe0a76648eda7a2db5698d5782b7a1c260
igor <igor.patramanis@gmail.com>
2025-11-22 11:24:42 +0200

    Create user controller, create users resource route
    Register api guard, add token expiration time 24 hours

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Users routes
+    Route::resource('users', UserController::class);
+});
