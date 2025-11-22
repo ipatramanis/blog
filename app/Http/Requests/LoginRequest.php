b5fca444d996ec3484604878fa000dcb05845aee
igor <igor.patramanis@gmail.com>
2025-11-22 12:15:44 +0200

    Create form request validations for login and register

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email:rfc,spoof',
+            'password' => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'email field is required.',
+            'email.email' => 'Invalid credentials.',
+            'password.required' => 'password field is required.',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        // Format error messages
+        $errorMessages = [];
+        foreach ($validator->errors()->getMessageBag()->getMessages() as $key => $message) {
+            $errorMessages['message'][$key] = $message[0];
+        }
+
+        // Throw json response with http status
+        throw new HttpResponseException(
+            response()->json($errorMessages, 422)
+        );
+    }
+}
---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email:rfc,spoof|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8',
+            'repeat_password' => 'required|string|min:8|same:password',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.email' => 'Invalid email address.',
+            'email.unique' => 'Invalid email address.',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        // Format error messages
+        $errorMessages = [];
+        foreach ($validator->errors()->getMessageBag()->getMessages() as $key => $message) {
+            $errorMessages['message'][$key] = $message[0];
+        }
+
+        // Throw json response with http status
+        throw new HttpResponseException(
+            response()->json($errorMessages, 422)
+        );
+    }
+}
