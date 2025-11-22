51375e8a979153beb55d8571ce0ee16f542fbc2f
igor <igor.patramanis@gmail.com>
2025-11-22 12:16:17 +0200

    Create routes for register, login and logout

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Authentication routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware(['auth:sanctum'])->group(function () {
+    // Authentication routes
+    Route::post('/logout', [AuthController::class, 'logout']);
+
     // Users routes
     Route::resource('users', UserController::class);
 });
