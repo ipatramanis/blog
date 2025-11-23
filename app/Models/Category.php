8e573a0500d4703a09f8475ffcd386ba3c32a93e
igor <igor.patramanis@gmail.com>
2025-11-23 18:45:39 +0200

    Add fillable field to Category model, parent_id and title

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'parent_id',
+        'title',
+    ];
+
+    /**
      * Get the posts that belongs to the category
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
